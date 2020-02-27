18a69f93c1ff401194556585d7fa7d0932d46eaf
Cam at Home <51334610+industrial-plankton@users.noreply.github.com>
2020-02-26 18:30:17 -0800

    cleanup

--- backend/bird_handlers.go
+++ backend/bird_handlers.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"backend/IPDatabase"
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"strings"
 
 	"github.com/golang/gddo/httputil/header"
-	//json "github.com/json-iterator/go" //
-	"encoding/json"
+	json "github.com/json-iterator/go" //
+	//	"encoding/json"
 )
 
 type table struct {
@@ -47,9 +42,6 @@ func getBirdHandler(w http.ResponseWriter, r *http.Request) {
 func tokensignin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
 
-	var token auth
-	//utility.CheckJSON(w, r, token)
-
 	// If the Content-Type header is present, check that it has the value
 	// application/json. Note that we are using the gddo/httputil/header
 	// package to parse and extract the value here, so the check works
@@ -77,65 +69,11 @@ func tokensignin(w http.ResponseWriter, r *http.Request) {
 	dec := json.NewDecoder(r.Body)
 	dec.DisallowUnknownFields()
 
+	var token auth
 	err := dec.Decode(&token)
 	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-
-		switch {
-		// Catch any syntax errors in the JSON and send an error message
-		// which interpolates the location of the problem to make it
-		// easier for the client to fix.
-		case errors.As(err, &syntaxError):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// In some circumstances Decode() may also return an
-		// io.ErrUnexpectedEOF error for syntax errors in the JSON. There
-		// is an open issue regarding this at
-		// https://github.com/golang/go/issues/25956.
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON")
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// Catch any type errors, like trying to assign a string in the
-		// JSON request body to a int field in our Person struct. We can
-		// interpolate the relevant field name and position into the error
-		// message to make it easier for the client to fix.
-		case errors.As(err, &unmarshalTypeError):
-			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// Catch the error caused by extra unexpected fields in the request
-		// body. We extract the field name from the error message and
-		// interpolate it in our custom error message. There is an open
-		// issue at https://github.com/golang/go/issues/29035 regarding
-		// turning this into a sentinel error.
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// An io.EOF error is returned by Decode() if the request body is
-		// empty.
-		case errors.Is(err, io.EOF):
-			msg := "Request body must not be empty"
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// Catch the error caused by the request body being too large. Again
-		// there is an open issue regarding turning this into a sentinel
-		// error at https://github.com/golang/go/issues/30715.
-		case err.Error() == "http: request body too large":
-			msg := "Request body must not be larger than 1MB"
-			http.Error(w, msg, http.StatusRequestEntityTooLarge)
-
-		// Otherwise default to logging the error and sending a 500 Internal
-		// Server Error response.
-		default:
-			log.Println(err.Error())
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		}
-		return
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
 	// Check that the request body only contained a single JSON object.
@@ -145,26 +83,9 @@ func tokensignin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "JSON Data: %+v", token)
-
-	fmt.Println("ResponseWriter: ")
-	fmt.Println(w)
 	fmt.Println("Token: ")
 	fmt.Println(token)
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(r.Body)
-	newStr := buf.String()
-
-	fmt.Println("r.Body: ", string(newStr))
-
-	fmt.Println("request: ")
-	fmt.Println(r)
-
-	w.WriteHeader(http.StatusAccepted)
-
-	fmt.Println("Validation URL: ")
-	fmt.Println("https://oauth2.googleapis.com/tokeninfo?id_token=" + token.Idtoken)
 	resp, err := http.Get("https://oauth2.googleapis.com/tokeninfo?id_token=" + token.Idtoken)
 	if err != nil {
 		fmt.Println(err)
